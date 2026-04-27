f47c86340b57320e0fa4164efecbb6029015e2e6
Maria Lemos <oi@marialemos.com>
2026-04-27 20:37:06 -0300

    feat: navigation

--- packages/design-system/src/molecules/Navigation/Navigation.tsx
+++ packages/design-system/src/molecules/Navigation/Navigation.tsx
@@ -1,10 +1,30 @@
-import { ThemeToggle } from "../../atoms"
-import { SectionBox } from "../SectionBox/SectionBox"
+import { ThemeToggle } from "../../atoms";
+import { SectionBox } from "../SectionBox/SectionBox";
+import Link from "next/link";
+import "./Navigation.css";
+
+const pagesList = [
+  { name: "Home", path: "/" },
+  { name: "Transferencias", path: "/transfer" },
+  { name: "Investimentos", path: "/investments" },
+  { name: "Outros serviços", path: "/services" },
+];
 
 export const Navigation = ({ className }: { className?: string }) => {
-
-    return (<SectionBox className={`navigation ${className}`}><nav>
-      
-    </nav>
-    <ThemeToggle /></SectionBox>)
-}
+  return (
+    <SectionBox
+      className={`flex flex-col justify-between items-center navigation ${className}`}
+    >
+      <nav>
+        <ul className="flex navigation divide-primary divide-solid divide-y-2">
+          {pagesList.map((page) => (
+            <li key={page.path} className="px-4 pt-4 pb-2">
+              <Link href={page.path}>{page.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <ThemeToggle />
+    </SectionBox>
+  );
+};
--- packages/design-system/src/molecules/SectionBox/SectionBox.tsx
+++ packages/design-system/src/molecules/SectionBox/SectionBox.tsx
@@ -4,24 +4,31 @@ import "./SectionBox.css";
 
 export const SectionBox = ({
   children,
-  className = "",title, variant = "colored",
+  className = "",
+  title,
+  variant = "colored",
 }: {
   children: React.ReactNode;
   title?: React.ReactNode;
   className?: string;
   variant?: "bg" | "colored";
 }) => {
- 
   return (
     <section
       className={`sectionBox p-8 ${variant} ${variant === "bg" ? "" : "bg-foreground"} text-font rounded-md w-full ${className}`}
-      style={variant === "bg" ? { 
-        '--bg-image': `url(${image.src || image.default || image})` 
-      } as React.CSSProperties : undefined}
+      style={
+        variant === "bg"
+          ? ({
+              "--bg-image": `url(${image.src || image.default || image})`,
+            } as React.CSSProperties)
+          : undefined
+      }
     >
-      <Typography variant="h2" className="mb-2">
-        {title}
-      </Typography>
+      {title && (
+        <Typography variant="h2" className="mb-2">
+          {title}
+        </Typography>
+      )}
       {children}
     </section>
   );
